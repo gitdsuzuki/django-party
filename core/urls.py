62555c17e84e6d3a22026c05b5f511d5546e43a7
Daichi Suzuki <genshiroku@gmail.com>
2026-01-07 22:24:36 +0900

    Party List page created.

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 # core/urls.py
+
 from django.contrib import admin
 from django.urls import include, path
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
+    path("", include("party.urls")),  # NEW
 ]
